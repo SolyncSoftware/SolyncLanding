04c751c117f782696c6c155ceb04f1684cc2a5f3
papertek <littlerock894@gmail.com>
2026-02-26 00:25:23 -0600

    test:

--- src/routes/legal/+page.svelte
+++ src/routes/legal/+page.svelte
@@ -2,7 +2,7 @@
 <p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
-<h1 class="text-3xl font-bold underline">Hello world! this is the homepage</h1>
+<h1 class="text-3xl font-bold underline">Hello world! this is the legal page</h1>
 
 <style lang="postcss">
     @reference "tailwindcss";
