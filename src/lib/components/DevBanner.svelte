9d4249ec8b601bf2b599893ba11e051766c52c41
papertek <littlerock894@gmail.com>
2026-02-28 01:04:03 -0600

    feat:

---
+++ src/lib/components/DevBanner.svelte
@@ -0,0 +1,5 @@
+<div class="sticky bottom-0 left-0 z-40 bg-black">
+    <div class="p-1 w-full flex gap-2 items-center justify-center text-white/80">
+        Development build of NetroWorks. Strictly for internal use. No leaking just yet!
+    </div>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,7 @@
     import "../styles/tailwind.css";
     import Header from "$lib/components/Header.svelte";
     import Footer from "$lib/components/Footer.svelte";
+    import DevBanner from "$lib/components/DevBanner.svelte";
 </script>
 
 <Header />
@@ -10,3 +11,4 @@
     {@render children()}
 </main>
 <Footer />
+<DevBanner />
