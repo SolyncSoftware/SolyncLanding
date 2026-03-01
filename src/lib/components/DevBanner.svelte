83375f8b607ee10501e6371c80e74d6a914cc9a6
papertek <littlerock894@gmail.com>
2026-03-01 06:45:26 -0600

    feat: arrow and formatting stuff

--- src/lib/components/DevBanner.svelte
+++ src/lib/components/DevBanner.svelte
@@ -1,5 +1,5 @@
 <div class="sticky bottom-0 left-0 z-40 bg-black">
-    <div class="p-1 w-full flex gap-2 items-center justify-center text-white/80">
+    <div class="p-1 w-full gap-2 flex items-center justify-center text-white/80">
         Development build of NetroWorks. Strictly for internal use. No leaking just yet!
     </div>
 </div>
