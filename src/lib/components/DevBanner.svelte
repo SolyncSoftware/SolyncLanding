dddacdf07616022d497ec730cde9a859d9193940
papertek <littlerock894@gmail.com>
2026-03-01 06:55:10 -0600

    lol

--- src/lib/components/DevBanner.svelte
+++ src/lib/components/DevBanner.svelte
@@ -1,5 +1,5 @@
 <div class="sticky bottom-0 left-0 z-40 bg-black">
-    <div class=" flex w-full items-center justify-center gap-2 p-1 text-white/80">
+    <div class="flex w-full items-center justify-center gap-2 p-1 text-white/80">
         Development build of NetroWorks. Strictly for internal use. No leaking just yet!
     </div>
 </div>
