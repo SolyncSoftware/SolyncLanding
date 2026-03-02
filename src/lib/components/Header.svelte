6de97134b86d8281e43bee2a2b668830a9a127a2
papertek <littlerock894@gmail.com>
2026-03-02 14:15:51 -0600

    style: header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,1 +1,7 @@
-<div class="bg-red-600">Header</div>
+<header class="font-display fixed top-0 left-0 z-50 w-full px-20 py-11.5">
+    <ol class="flex justify-end gap-13 text-xl">
+        <li><span class="font-extrabold">HOME</span></li>
+        <li><span>NEWS</span></li>
+        <li><span>SERVICES</span></li>
+    </ol>
+</header>
