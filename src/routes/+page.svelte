fc8af69f6ab9ce9edcd9addc6aac45316e5060d7
papertek <littlerock894@gmail.com>
2026-03-02 14:48:51 -0600

    feat(homepage): :sparkles: start homepage layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,21 @@
-<div>Background with waves and netro logo</div>
+<section class="relative flex h-screen items-end">
+    <div class="absolute inset-0 bg-cover bg-bottom bg-no-repeat" style="background-image: url('/waves.svg');"></div>
+
+    <div class="absolute z-10 flex w-full items-end justify-between px-16.5 py-19">
+        <div class="bg-black p-11">
+            <img src="/logo-blue.svg" alt="netro logo" />
+        </div>
+
+        <div class="flex flex-row items-center gap-4 bg-black pl-35.5">
+            <span class="font-display">SCROLL</span>
+            <img src="/arrow-default.svg" alt="arrow pointing down" />
+        </div>
+    </div>
+</section>
+
+<section class="bg-error p-8">junkkkkk</section>
+
+<section class="bg-error/30 relative">
+    <div class="absolute inset-0 bg-cover bg-bottom bg-no-repeat"></div>
+    <div class="relative z-10 flex h-full items-center justify-center">Block 2</div>
+</section>
