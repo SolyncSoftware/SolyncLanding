86c585d35d5cb6daeb91fa80fd209454d154cf4d
papertek <littlerock894@gmail.com>
2026-03-03 15:26:33 -0600

    feat: implement unicorn and waves

--- src/lib/components/index/HomeWaves.svelte
+++ src/lib/components/index/HomeWaves.svelte
@@ -4,7 +4,13 @@
 </script>
 
 <section class="relative -mx-16 flex h-screen items-end">
-    <div class="absolute inset-0 bg-cover bg-bottom bg-no-repeat" style="background-image: url(/waves.png);"></div>
+    <!-- <div class="absolute inset-0 bg-cover bg-bottom bg-no-repeat" style="background-image: url(/waves.png);"></div> -->
+    <div
+        class="unicorn-embed"
+        data-us-project-src="/netro_waves.json"
+        data-us-lazyload="true"
+        style="width: 100%; height: 100vh; image: url(/waves.png);"
+    ></div>
 
     <div class="absolute z-10 flex w-full items-end justify-between px-16.5 py-19">
         <div class="bg-black p-11">
