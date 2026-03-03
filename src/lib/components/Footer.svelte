f044c3095d95b57dd9e31791e26fb6eea95bcd25
papertek <littlerock894@gmail.com>
2026-03-03 00:01:35 -0600

    feat: implement footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,1 +1,35 @@
-<div class="bg-yellow-600">Footer</div>
+<footer class="bg-accent flex flex-col gap-8.5 px-16.5 py-9">
+    <div class="flex flex-row justify-between">
+        <span class="font-display text-[40px] font-bold text-black">&copy; Netro 2026</span>
+        <div class="flex flex-row gap-20">
+            <div class="flex flex-col gap-5 text-right text-2xl">
+                <a href="/services">Services</a>
+                <a href="/about">About us</a>
+                <a href="/apply">Apply to Netro</a>
+            </div>
+            <div class="flex flex-col gap-5 text-right text-2xl">
+                <a href="/contact">Contact us</a>
+                <a href="/terms">Terms of service</a>
+                <a href="/privacy">Privacy policy</a>
+                <a href="/support">NetroHost support</a>
+                <a href="/bug-report">Report a bug</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="flex flex-row justify-between">
+        <img src="/logo-black.svg" alt="netro logo" class="self-start" />
+        <a href="https://untone.org/" class="font-display flex gap-3 self-end"
+            >Design assistance by <img src="/untone.svg" alt="untone logo" class="max-h-6" /></a
+        >
+    </div>
+</footer>
+
+<style scoped>
+    a {
+        color: #9ca3af;
+        &:hover {
+            color: #e5e7eb;
+        }
+    }
+</style>
