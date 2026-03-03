ea5bd961b45176e82967f9e6b4be01b0fdcb7d5a
papertek <littlerock894@gmail.com>
2026-03-02 21:39:39 -0600

    fix: fix banner being wonk

--- src/lib/components/DevBanner.svelte
+++ src/lib/components/DevBanner.svelte
@@ -1,4 +1,4 @@
-<div class="bg-accent/30 sticky bottom-0 left-0 z-40">
+<div class="bg-accent/70 sticky bottom-0 left-0 z-100">
     <div class="flex w-full items-center justify-center gap-2 p-1 text-white/80">
         Development build of NetroWorks. Strictly for internal use. No showing off just yet!
     </div>
