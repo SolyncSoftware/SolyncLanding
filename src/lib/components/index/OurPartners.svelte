53eab8ed1df6faa34758e3b935b379fb679b95ab
papertek <littlerock894@gmail.com>
2026-03-04 05:38:48 -0600

    feat(homepage): :sparkles: implement our partners

---
+++ src/lib/components/index/OurPartners.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+    interface Partner {
+        src: string;
+        alt: string;
+        href?: string;
+    }
+    export let partners: Partner[] = [
+        {
+            src: '/untone.svg',
+            alt: 'Untone logo',
+            href: 'https://untone.org/'
+        }
+    ];
+</script>
+
+<section class="pb-15">
+    <span class="font-display text-[140px]">OUR <span class="text-accent">PARTNERS</span></span>
+    <div class="grid grid-cols-3 gap-5">
+        {#each partners as partner}
+            <div class="relative h-50 overflow-hidden border border-[#222222] px-12">
+                {#if partner.href}
+                    <a
+                        href={partner.href}
+                        target="_blank"
+                        class="absolute inset-0 z-10 flex items-center justify-center"
+                        aria-label={partner.alt}
+                    >
+                    </a>
+                {/if}
+                <img src={partner.src} alt={partner.alt} class="h-full w-full object-contain" />
+            </div>
+        {/each}
+    </div>
+</section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
     import HomeWaves from '$lib/components/index/HomeWaves.svelte';
+    import OurPartners from '$lib/components/index/OurPartners.svelte';
     import WeAreNetro from '$lib/components/index/WeAreNetro.svelte';
     import WhatWeDo from '$lib/components/index/WhatWeDo.svelte';
     import WhoWeAre from '$lib/components/index/WhoWeAre.svelte';
@@ -11,4 +12,5 @@
     <WeAreNetro />
     <WhatWeDo />
     <WhoWeAre />
+    <OurPartners />
 </div>
