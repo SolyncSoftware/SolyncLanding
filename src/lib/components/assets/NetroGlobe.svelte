4383c0c60279c275e69108960360c4aa108e37c8
co00k1e <cookiespenceremail@gmail.com>
2026-03-21 14:31:55 +0000

    NW-14: Extraction of globe into its own component

---
+++ src/lib/components/assets/NetroGlobe.svelte
@@ -0,0 +1,7 @@
+<div
+    class="unicorn-embed"
+    data-us-project-src="/netro_glyph.json"
+    data-us-lazyload="true"
+    data-us-scale="0.8"
+    style="width: 100%; height: 100vh; image: url(/logo-ascii.png);"
+></div>
--- src/lib/components/index/WeAreNetro.svelte
+++ src/lib/components/index/WeAreNetro.svelte
@@ -1,3 +1,7 @@
+<script>
+    import NetroGlobe from '$lib/components/assets/NetroGlobe.svelte';
+</script>
+
 <section class="relative mt-7 flex items-center justify-between">
     <div class="z-10 pb-22">
         <span class="font-display text-[140px]">WE ARE <span class="text-accent font-extrabold">NETRO</span></span>
@@ -12,12 +16,6 @@
     </div>
 
     <div class="absolute inset-y-0 right-0 bottom-0 -z-100 h-full w-1/2 translate-x-16 -translate-y-43">
-        <div
-            class="unicorn-embed"
-            data-us-project-src="/netro_glyph.json"
-            data-us-lazyload="true"
-            data-us-scale="0.8"
-            style="width: 100%; height: 100vh; image: url(/logo-ascii.png);"
-        ></div>
+        <NetroGlobe />
     </div>
 </section>
