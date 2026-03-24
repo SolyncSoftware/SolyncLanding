68706648f1d64976a7f7b2fb81433c0f76aab250
papertek <littlerock894@gmail.com>
2026-03-24 13:01:33 -0500

    fix: :bug: improve footer code with waves component

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -3,9 +3,11 @@
     import Waves from '$lib/components/assets/Waves.svelte';
 </script>
 
-<footer class="relative">
-    <Waves className="absolute -z-100" style="height: 100%; overflow: hidden;" wavesType="/netro_waves.json" />
-    <div class="flex flex-col gap-7 px-16.5 py-9">
+<footer class="relative overflow-hidden">
+    <div class="col-start-1 row-start-1 h-full w-full">
+        <Waves style="position: absolute; top: 0; left: 0; width: 100vw; height: 100vh; z-index: -110;" wavesType="/netro_waves.json" />
+    </div>
+    <div class="col-start-1 row-start-1 flex flex-col gap-7 px-16.5 py-9">
         <div class="flex flex-row justify-between">
             <span class="font-display text-[40px] font-bold text-white">&copy; Netro 2026</span>
             <div class="flex flex-row gap-20">
