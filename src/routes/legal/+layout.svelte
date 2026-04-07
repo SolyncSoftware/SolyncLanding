814233991b79d5786a62eef6e7ca2defb96053ee
papertek <littlerock894@gmail.com>
2026-04-06 21:42:34 -0500

    fix(legal): :sparkles: redo legal pages to use root layout (needs review)

--- src/routes/legal/+layout.svelte
+++ src/routes/legal/+layout.svelte
@@ -2,16 +2,24 @@
     import { page } from '$app/state';
     let { children } = $props();
     const title = page.data.title;
+
+    console.log(page.data);
 </script>
 
-<section class="-mx-16 grid min-h-screen">
+<!-- <section class="-mx-16 grid min-h-screen">
     <div class="z-10 col-start-1 row-start-1 flex flex-col items-start gap-12 p-21">
-        <!-- page content -->
-        <h1 class="font-display text-5xl leading-16 font-bold">{title}</h1>
+        <h1 class="font-display text-5xl leading-16 font-bold">{title}Placeholder</h1>
         <div class="border-t-accent h-auto w-301 border-t-11 bg-black p-6">
             <span class="font-sans text-2xl">
                 {@render children()}
             </span>
         </div>
     </div>
-</section>
+</section> -->
+
+<h1 class="text-5xl font-bold">VERY COOL TITLE</h1>
+<div class="border-t-accent mt-6 h-auto w-301 border-t-11 bg-black p-8">
+    <span class="font-sans text-2xl">
+        {@render children()}
+    </span>
+</div>
