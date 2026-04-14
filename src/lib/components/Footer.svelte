e679126ac0f099b61baf0eec84a6f0fce28b8d3a
papertek <littlerock894@gmail.com>
2026-04-13 21:13:16 -0500

    fix: :ambulance: fix the z-index

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -6,7 +6,7 @@
 <footer class="relative overflow-hidden">
     <!-- this is commented out for now until we get the json file -->
     <div class="col-start-1 row-start-1 h-full w-full">
-        <Waves style="position: absolute; top: 0; left: 0; width: 100vw; height: 100vh; z-index: -110;" wavesType="/netro_waves.json" />
+        <Waves style="position: absolute; top: 0; left: 0; width: 100vw; height: 100vh; z-index: -1;" wavesType="/netro_footer.json" />
     </div>
     <div class="col-start-1 row-start-1 flex flex-col gap-7 px-16 py-9">
         <div class="flex flex-row justify-between">
