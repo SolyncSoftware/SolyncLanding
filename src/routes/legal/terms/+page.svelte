4a733d90db73f0d825717e98e7e804fc67d14883
Oddbyte <git@oddbyte.dev>
2026-04-19 13:11:02 -0400

    oop i forgor to update the TOS page.svelte, doing it now

--- src/routes/legal/terms/+page.svelte
+++ src/routes/legal/terms/+page.svelte
@@ -1,30 +1,6 @@
 <script lang="ts">
+    import LegalPage from '$lib/components/Legal.svelte';
     let { data } = $props();
 </script>
 
-<p class="text-accent">Last revised {data.revised}</p>
-
-<div class="markdown">
-    {@html data.html}
-</div>
-
-<style>
-    .markdown :global(h2) {
-        font-family: var(--font-display);
-        margin-top: 1.5rem;
-        margin-bottom: 1.5rem;
-        font-size: 1.875rem;
-        line-height: 2.25rem;
-        font-weight: 700;
-    }
-
-    .markdown :global(p) {
-        margin-bottom: 0.5rem;
-    }
-
-    .markdown :global(ul) {
-        margin-bottom: 0.5rem;
-        margin-left: 1.5rem;
-        list-style-type: disc;
-    }
-</style>
+<LegalPage {data} />
