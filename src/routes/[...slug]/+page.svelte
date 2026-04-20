4f1bcf830047e6c84880a2e3c4908ce15f769b0d
papertek <littlerock894@gmail.com>
2026-04-20 05:21:32 -0500

    feat(site): :sparkles: switch to new markdown system

---
+++ src/routes/[...slug]/+page.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+    let { data } = $props();
+</script>
+
+<div class="border-t-accent mt-6 h-auto w-301 border-t-11 bg-black p-8">
+    <span class="font-sans text-2xl">
+        <p class="text-accent">Written/Revised on {data.meta.date}</p>
+
+        <div class="markdown">
+            <data.content />
+        </div>
+    </span>
+</div>
+
+<style>
+    .markdown :global(h2) {
+        font-family: var(--font-display);
+        margin-top: 1.5rem;
+        margin-bottom: 1.5rem;
+        font-size: 1.875rem;
+        line-height: 2.25rem;
+        font-weight: 700;
+    }
+
+    .markdown :global(p) {
+        margin-bottom: 0.5rem;
+    }
+
+    .markdown :global(ul) {
+        margin-bottom: 0.5rem;
+        margin-left: 1.5rem;
+        list-style-type: disc;
+    }
+</style>
--- src/routes/api/articles/+server.ts
+++ src/routes/api/articles/+server.ts
@@ -4,11 +4,11 @@ import type { Article } from '$lib/utils/types.ts';
 async function getArticles() {
     let articles: Article[] = [];
 
-    const paths = import.meta.glob('/src/articles/*.md', { eager: true });
+    const paths = import.meta.glob('/src/articles/**/*.md', { eager: true });
 
     for (const path in paths) {
         const file = paths[path];
-        const slug = path.split('/').at(-1)?.replace('.md', '');
+        const slug = path.replace('/src/articles/', '').replace(/\.md$/, '');
 
         if (file && typeof file === 'object' && 'metadata' in file && slug) {
             const metadata = file.metadata as Omit<Article, 'slug'>;
