c8fe4165a6e1cb8943d5e25843256c8b3ff45d0e
Tanza3D <archie@untone.uk>
2026-04-20 15:40:32 +0100

    Improve performance of unicorn views

--- src/lib/components/assets/Waves.svelte
+++ src/lib/components/assets/Waves.svelte
@@ -9,5 +9,7 @@
     data-us-project-src={wavesType}
     data-us-lazyload="true"
     data-us-scale="0.8"
+    data-us-dpi="1"
+    data-us-fps="60"
     style={`width: 100%; background-image: url(/waves.png); ${style}`}
 ></div>
