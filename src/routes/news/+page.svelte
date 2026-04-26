367c8683048ee23bfdbf29b3748239aa15ba05c3
papertek <littlerock894@gmail.com>
2026-04-26 06:55:49 -0500

    feat(news): :sparkles: more work on the news homepage

--- src/routes/news/+page.svelte
+++ src/routes/news/+page.svelte
@@ -1,8 +1,14 @@
 <script lang="ts">
     import LargePost from '$lib/components/news/LargePost.svelte';
+    import SmallPost from '$lib/components/news/SmallPost.svelte';
 </script>
 
 <h1 class="text-5xl font-bold">LATEST NEWS</h1>
-<div class="mt-6 max-w-301">
+<div class="mt-6 flex max-w-301 flex-col gap-12">
     <LargePost />
+
+    <div class="flex flex-row gap-8">
+        <SmallPost />
+        <SmallPost />
+    </div>
 </div>
