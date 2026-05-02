c63185645e0adb927b6459f045d7fd489e95243a
papertek <littlerock894@gmail.com>
2026-05-02 03:49:17 -0500

    fix: fix header spanning width of page

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,5 +1,5 @@
 <!-- todo: make it dynamic  -->
-<header class="px-21 pt-21">
+<header class="w-fit px-21 pt-21">
     <a href="/">
         <img src="/images/logo-blue.svg" alt="netro logo" width="540" height="118" />
     </a>
