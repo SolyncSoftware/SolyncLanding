7913676de51a17d5a0f81a9f5cb2f74d056abb67
papertek <littlerock894@gmail.com>
2026-05-01 23:49:45 -0500

    perf(homepage): add the optimized glyph but dont use it because it doesnt look that good

--- src/lib/components/assets/NetroGlobe.svelte
+++ src/lib/components/assets/NetroGlobe.svelte
@@ -4,7 +4,7 @@
 
 <div
     class="unicorn-embed"
-    data-us-project-src="/netro_glyph.json"
+    data-us-project-src="/netro_glyph_old.json"
     data-us-lazyload="true"
     data-us-scale="0.8"
     style={`width: 100%; height: 100vh; objectFit: 'contain';image: url(/logo-ascii.png); ${style}`}
