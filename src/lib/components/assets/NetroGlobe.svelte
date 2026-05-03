37c9a57b92e4da3166fe3b23554f7c116a479f7d
papertek <littlerock894@gmail.com>
2026-05-03 14:36:14 -0500

    fix: fix fps

--- src/lib/components/assets/NetroGlobe.svelte
+++ src/lib/components/assets/NetroGlobe.svelte
@@ -7,7 +7,7 @@
     export let backgroundImage = '/images/logo-ascii.png';
     export let backgroundSize = 'contain';
     export let autoFallback = true;
-    export let fpsThreshold = 15;
+    export let fpsThreshold = 45;
     export let sampleDurationMs = 2000;
 
     let useWebgl = false;
