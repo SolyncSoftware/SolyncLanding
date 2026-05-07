de52d67b325a3e2b225ceab74c9272b6d54980d6
papertek <littlerock894@gmail.com>
2026-05-07 05:51:12 -0500

    style: buttonsimple looks ok

--- src/lib/components/ButtonSimple.svelte
+++ src/lib/components/ButtonSimple.svelte
@@ -4,12 +4,12 @@
 
 <a {href} class={`group relative flex flex-row items-center gap-4 transition-all ${className}`} {...rest}>
     <span class="relative inline-flex items-center gap-4">
-        <!-- <span class="absolute -inset-1.5 -z-10 group-hover:bg-white/15"></span> -->
+        <span class="absolute -inset-2 -z-10 transition-all duration-300 group-hover:bg-black"></span>
 
         <span class="relative">
             <span class="transform transition-all duration-300">{text}</span>
-            <span class="absolute bottom-0 left-0 h-0.5 w-0 bg-white/40 transition-all duration-300 group-hover:w-full"></span>
+            <span class="absolute bottom-0 left-0 h-0.5 w-0 bg-white/30 transition-all duration-300 group-hover:w-full"></span>
         </span>
-        <img src="/images/arrow-default.png" alt="arrow pointing right" class="transition-transform duration-300" />
+        <img src="/images/arrow-default.png" alt="arrow pointing right" class="transition-transform duration-900" />
     </span>
 </a>
