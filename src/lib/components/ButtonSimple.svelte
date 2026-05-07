23d01717f096e9219b3b0dc7efbd301944379f06
papertek <littlerock894@gmail.com>
2026-05-07 06:03:25 -0500

    style: improve buttonsimple's hover design

--- src/lib/components/ButtonSimple.svelte
+++ src/lib/components/ButtonSimple.svelte
@@ -3,13 +3,17 @@
 </script>
 
 <a {href} class={`group relative flex flex-row items-center gap-4 transition-all ${className}`} {...rest}>
-    <span class="relative inline-flex items-center gap-4">
-        <span class="absolute -inset-2 -z-10 transition-all duration-300 group-hover:bg-black"></span>
+    <span class="relative z-100 inline-flex items-center gap-4">
+        <span
+            class="group-hover:outline-accent absolute -inset-2 -z-10 outline-2
+                   outline-white/0 transition-all duration-300
+                   ease-in-out group-hover:bg-black group-hover:outline-2"
+        ></span>
 
         <span class="relative">
             <span class="transform transition-all duration-300">{text}</span>
-            <span class="absolute bottom-0 left-0 h-0.5 w-0 bg-white/30 transition-all duration-300 group-hover:w-full"></span>
         </span>
+
         <img src="/images/arrow-default.png" alt="arrow pointing right" class="transition-transform duration-900" />
     </span>
 </a>
