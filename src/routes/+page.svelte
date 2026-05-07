3dc11c1a210df5cf0b24d6d2a3477b7a65b49daa
papertek <littlerock894@gmail.com>
2026-05-07 16:51:18 -0500

    fix: fix services cards not spanning

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -44,7 +44,7 @@
         </div>
     </div>
 
-    <div class="col-start-1 row-start-2 mt-12 flex w-fit flex-col gap-5">
+    <div class="col-start-1 row-start-2 mt-12 flex flex-col gap-5 xl:w-fit">
         <div class="flex flex-col gap-12 xl:flex-row xl:gap-8">
             <!-- todo: refactor into component and endpoint -->
             <a
