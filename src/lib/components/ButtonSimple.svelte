42e180e0914ad33cf4bff57196b7cd0c8d74855b
papertek <littlerock894@gmail.com>
2026-05-07 01:38:43 -0500

    style: make simple links reactive

--- src/lib/components/ButtonSimple.svelte
+++ src/lib/components/ButtonSimple.svelte
@@ -2,7 +2,14 @@
     let { class: className = '', href = '#', text = 'add message', ...rest } = $props();
 </script>
 
-<a {href} class={`flex flex-row items-center gap-4 hover:opacity-70 ${className}`}>
-    <span class="font-display uppercase">{text}</span>
-    <img src="/images/arrow-default.png" alt="arrow pointing right" />
+<a {href} class={`group relative flex flex-row items-center gap-4 transition-all ${className}`} {...rest}>
+    <span class="relative inline-flex items-center gap-4">
+        <!-- <span class="absolute -inset-1.5 -z-10 group-hover:bg-white/15"></span> -->
+
+        <span class="relative">
+            <span class="transform transition-all duration-300">{text}</span>
+            <span class="absolute bottom-0 left-0 h-0.5 w-0 bg-white/40 transition-all duration-300 group-hover:w-full"></span>
+        </span>
+        <img src="/images/arrow-default.png" alt="arrow pointing right" class="transition-transform duration-300" />
+    </span>
 </a>
