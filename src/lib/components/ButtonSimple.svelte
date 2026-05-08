596395ad75489e1f944ef2dd502d57f1422433c7
Meow <github@xela.codes>
2026-05-08 01:25:17 -0400

    fix(homepage): :lipstick: anchor button expansion to center

--- src/lib/components/ButtonSimple.svelte
+++ src/lib/components/ButtonSimple.svelte
@@ -9,7 +9,7 @@
     <span class="pointer-events-none invisible block h-0 pr-8 pl-4 tracking-[10%]">{text}</span>
     <!-- actual button: -->
     <a {href} class={['group relative flex flex-row items-center gap-4 transition-all', className]} {...rest}>
-        <span class="relative z-10 inline-flex w-max flex-nowrap items-center gap-4">
+        <span class="relative z-10 mx-auto inline-flex w-max flex-nowrap items-center gap-4">
             <span
                 class="whitespace-nowrap transition-all duration-300
                    group-hover:tracking-[10%]"
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -40,7 +40,7 @@
 
         <div class="flex flex-col items-start gap-4 text-xl md:flex-row">
             <ButtonSimple text="LEARN MORE" href="/about" />
-            <ButtonSimple text="JOIN US" href="/apply" class="[&>span]:ml-auto" />
+            <ButtonSimple text="JOIN US" href="/apply" />
         </div>
     </div>
 
