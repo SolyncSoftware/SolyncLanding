fe66f522cc9d661a1b7e8dfaaf116a51dc37cc06
papertek <littlerock894@gmail.com>
2026-05-24 15:23:56 -0500

    chore: undo i was in the wrong repo

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,5 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    plugins: [sveltekit(), tailwindcss()],
-    server: {
-        allowedHosts: ['potaro.wiki', '.potaro.wiki']
-    }
+    plugins: [sveltekit(), tailwindcss()]
 });
