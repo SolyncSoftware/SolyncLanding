f7dd8b33a2770c65cd2aacc2f746873a688f59d4
papertek <littlerock894@gmail.com>
2026-06-02 22:34:02 -0500

    feat: :sparkles: implement new header

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -4,9 +4,8 @@
 
 <a
     {href}
-    class={`bg-accent group font-display outline-accent hover:outline-accent flex items-center gap-4 self-end py-4 pr-4 pl-7 text-lg font-bold uppercase outline-2 -outline-offset-2 transition-all ease-in-out hover:bg-black ${className}`}
+    class={`bg-offaccent group flex items-center gap-4 self-end rounded-full px-8 py-3 text-xl text-white transition-all ease-in-out hover:bg-black ${className}`}
     {...rest}
 >
     {text}
-    <img src="/images/arrow-default.png" alt="arrow pointing right" />
 </a>
--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,11 +1,32 @@
 <script lang="ts">
+    import { page } from '$app/state';
+
     import SolyncLogo from './SolyncLogo.svelte';
+    import Button from './Button.svelte';
+    import ButtonSimple from './ButtonSimple.svelte';
+
+    let currentPath = $derived(page.url.pathname);
+
+    function isActive(path: string): string {
+        return currentPath === path ? 'bg-white !text-accent font-display px-8 py-2 rounded-full' : '';
+    }
 </script>
 
-<header class="3xl:max-w-560 mx-auto w-full px-5 pt-10 lg:px-21 lg:pt-21">
-    <a href="/" class="group inline-block">
-        <SolyncLogo
-            class="fill-accent w-70  transition-all duration-300 ease-in-out group-hover:scale-102 group-hover:fill-white lg:w-135 lg:md:h-29.5"
-        />
-    </a>
+<header class="bg-accent flex items-center px-5 pt-6 pb-40 xl:px-70">
+    <nav class="flex items-center gap-7">
+        <a href="/" class="group inline-block">
+            <SolyncLogo
+                iconOnly={true}
+                class="w-20 fill-white transition-all duration-300 ease-in-out group-hover:scale-102 group-hover:fill-black"
+            />
+        </a>
+
+        <ButtonSimple href="/" text="Home" class={`text-white hover:text-black ${isActive('/')}`} />
+        <ButtonSimple href="/donate" text="Donate" class={`text-white hover:text-black ${isActive('/donate')}`} />
+        <ButtonSimple href="/about" text="Learn more" class={`text-white hover:text-black ${isActive('/about')}`} />
+    </nav>
+
+    <div class="ml-auto flex justify-end">
+        <Button href="https://orbit.solync.org" text="Manage Account" />
+    </div>
 </header>
