d4c60826567ab1a9ccfb3c1ef505d451efde6ef6
papertek <littlerock894@gmail.com>
2026-06-03 20:18:57 -0500

    chore: more wave updates

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -21,17 +21,18 @@
 </svelte:head>
 
 <section class="flex">
-    <div class="border-offwhite relative w-full rounded-4xl border-4 p-12 text-white">
+    <div class="border-offwhite relative w-full overflow-hidden rounded-4xl border-4 text-white">
         <Waves
-            className="absolute inset-0 z-10 h-full w-full"
+            className="lg:h-full h-fit overflow-hidden"
+            style="z-index: -1; overflow: hidden;"
             wavesType="/solync_waves.json"
             backgroundImage="/images/waves.png"
-            backgroundSize="contain"
+            backgroundSize="cover"
         />
 
-        <div class="relative z-10">
+        <div class="relative z-10 p-12">
             <SolyncLogo class="mb-24 w-110 fill-white" />
-            <h1 class="font-display text-4xl md:text-5xl">Building what comes next, together.</h1>
+            <h1 class="text-4xl font-extrabold md:text-5xl">Building what comes next, together.</h1>
             <p class="mt-4 text-lg md:max-w-170 md:text-xl">We're Solync. We create software like StreamSkinner, Gardens Wiki, and more.</p>
             <p class="mt-4 text-lg md:max-w-170 md:text-xl">
                 We're an independent software collective creating user-first experiences. It's not only because we love to, but we think
