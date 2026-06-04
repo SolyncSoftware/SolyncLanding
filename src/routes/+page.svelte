2d1c47cbd453ea3cb111e353c89931575d6b3980
papertek <littlerock894@gmail.com>
2026-06-03 19:20:32 -0500

    feat: begin work on homepage redesign

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -20,12 +20,21 @@
 </svelte:head>
 
 <section class="flex">
-    <span class="font-display text-lg md:max-w-206 md:text-xl">
+    <!-- <span class="font-display text-lg md:max-w-206 md:text-xl">
         <p class="md:max-w-200">We create software like StreamSkinner, Gardens Wiki, and more.</p>
         <br />
         <p>
             We're an independent software collective creating user-first experiences. It's not only because we love to, but we think it's
             ethical. We collaborate to create what's next.
         </p>
-    </span>
+    </span> -->
+
+    <div class="bg-accent w-full rounded-4xl border-4 border-white p-12 text-white">
+        <SolyncLogo class="mb-24 w-110 fill-white" />
+        <h1 class="font-display text-4xl md:text-5xl">Building what comes next, together.</h1>
+        <p class="mt-4 text-lg md:max-w-206 md:text-xl">
+            We create software like StreamSkinner, Gardens Wiki, and more. We're an independent software collective creating user-first
+            experiences. It's not only because we love to, but we think it's ethical. We collaborate to create what's next.
+        </p>
+    </div>
 </section>
