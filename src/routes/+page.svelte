7fa07ba1d3e66da2ae49cdf19096cc40588b4d11
papertek <littlerock894@gmail.com>
2026-06-05 00:08:51 -0500

    feat: begin work on donation page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
     import Button from '$lib/components/Button.svelte';
-    import Waves from '$lib/components/assets/Waves.svelte';
     import SolyncLogo from '$lib/components/SolyncLogo.svelte';
     import PageContainer from '$lib/components/assets/PageContainer.svelte';
     import OurTeam from '$lib/components/index/OurTeam.svelte';
@@ -27,8 +26,10 @@
     <PageContainer>
         <SolyncLogo class="mb-24 w-110 fill-white" />
         <h1 class="text-4xl font-bold md:text-5xl">Building what comes next, <span class="font-extrabold">together</span>.</h1>
-        <p class="mt-4 text-lg md:max-w-170 md:text-xl">We're Solync. We create software like StreamSkinner, Gardens Wiki, and more.</p>
-        <p class="mt-4 text-lg md:max-w-170 md:text-xl">
+        <p class="mt-4 text-lg font-semibold md:max-w-170 md:text-2xl">
+            We're Solync. We create software like StreamSkinner, Gardens Wiki, and more.
+        </p>
+        <p class="mt-4 text-lg font-semibold md:max-w-170 md:text-2xl">
             We're an independent software collective creating user-first experiences. It's not only because we love to, but we think it's
             ethical. We collaborate to create what's next.
         </p>
---
+++ src/routes/donate/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import PageContainer from '$lib/components/assets/PageContainer.svelte';
+</script>
+
+<PageContainer>
+    <h1 class="text-4xl font-bold md:text-5xl">Building what comes next, <span class="font-extrabold">together</span>.</h1>
+    <p class="mt-4 text-lg font-semibold md:max-w-170 md:text-2xl">
+        We're Solync. We create software like StreamSkinner, Gardens Wiki, and more.
+    </p>
+    <p class="mt-4 text-lg font-semibold md:max-w-170 md:text-2xl">
+        We're an independent software collective creating user-first experiences. It's not only because we love to, but we think it's
+        ethical. We collaborate to create what's next.
+    </p>
+</PageContainer>
