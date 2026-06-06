37fdd025977e58c79dbb2d6362aa993825d382ad
papertek <littlerock894@gmail.com>
2026-06-06 02:23:15 -0500

    feat: kofi widget

--- src/routes/donate/+page.svelte
+++ src/routes/donate/+page.svelte
@@ -3,12 +3,33 @@
 </script>
 
 <PageContainer>
-    <h1 class="text-4xl font-bold md:text-5xl">Building what comes next, <span class="font-extrabold">together</span>.</h1>
-    <p class="mt-4 text-lg font-semibold md:max-w-170 md:text-2xl">
-        We're Solync. We create software like StreamSkinner, Gardens Wiki, and more.
-    </p>
-    <p class="mt-4 text-lg font-semibold md:max-w-170 md:text-2xl">
-        We're an independent software collective creating user-first experiences. It's not only because we love to, but we think it's
-        ethical. We collaborate to create what's next.
-    </p>
+    <div class="flex flex-row justify-between">
+        <div>
+            <h1 class="text-4xl font-bold md:text-5xl">Build a better Solync.</h1>
+            <p class="mt-4 text-lg font-semibold md:max-w-170 md:text-2xl">
+                We're Solync. We create software like StreamSkinner, Gardens Wiki, and more.
+            </p>
+            <p class="mt-4 text-lg font-semibold md:max-w-170 md:text-2xl">
+                We're an independent software collective creating user-first experiences. It's not only because we love to, but we think
+                it's ethical. We collaborate to create what's next.
+            </p>
+        </div>
+        <div class="overflow-hidden rounded-3xl">
+            <iframe
+                id="kofiframe"
+                class="rounded-3xl"
+                src="https://ko-fi.com/solync/?hidefeed=true&widget=true&embed=true"
+                style="border:none;background:var(--color-white); box-shadow:none;"
+                width="400"
+                height="600"
+                title="solync"
+            ></iframe>
+        </div>
+    </div>
 </PageContainer>
+
+<style scoped>
+    .color-shit {
+        color: #ffffff00;
+    }
+</style>
