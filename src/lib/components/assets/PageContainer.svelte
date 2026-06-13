3b199577bda04e9715c0d5dda0537733aaff9384
papertek <littlerock894@gmail.com>
2026-06-13 06:20:45 -0500

    fix: fix up gradient and make it dynamic

--- src/lib/components/assets/PageContainer.svelte
+++ src/lib/components/assets/PageContainer.svelte
@@ -6,8 +6,8 @@
 
 <div class="border-offwhite relative z-100 w-full overflow-hidden rounded-4xl border-4 text-white">
     <Waves
-        className="lg:h-full h-fit overflow-hidden"
-        style="z-index: -1; overflow: hidden;"
+        className="lg:h-full h-fit overflow-hidden mix-blend-plus-lighter"
+        style="z-index: -1; overflow: hidden; mask-image: linear-gradient(to right, #fff6 20%, #fffa, #fff);"
         wavesType="/solync_waves_dark.json"
         backgroundImage="/images/waves-dark.png"
         backgroundSize="cover"
