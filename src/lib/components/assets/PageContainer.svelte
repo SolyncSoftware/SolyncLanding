7129e73395381663b0264ef8bb0b324b7fab327c
papertek <littlerock894@gmail.com>
2026-06-13 06:22:39 -0500

    fix: fix mobile stuff

--- src/lib/components/assets/PageContainer.svelte
+++ src/lib/components/assets/PageContainer.svelte
@@ -8,7 +8,7 @@
     <div class="absolute inset-0 z-1 bg-black/10"></div>
 
     <Waves
-        className="lg:h-full h-fit overflow-hidden mix-blend-plus-lighter"
+        className="h-full overflow-hidden mix-blend-plus-lighter"
         style="z-index: -1; overflow: hidden; mask-image: linear-gradient(to right, #fff6 20%, #fffa, #fff);"
         wavesType="/solync_waves_dark.json"
         backgroundImage="/images/waves-dark.png"
