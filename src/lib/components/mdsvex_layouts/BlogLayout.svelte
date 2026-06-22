b088f287443073ce700f6e20287f85bfd26b853e
papertek <littlerock894@gmail.com>
2026-06-21 22:14:40 -0500

    style: some more blog work

--- src/lib/components/mdsvex_layouts/BlogLayout.svelte
+++ src/lib/components/mdsvex_layouts/BlogLayout.svelte
@@ -42,7 +42,7 @@
         <span class="rounded-full bg-black/80 px-6 py-2 font-medium first-letter:uppercase">{categories[0]}</span>
 
         <div class="flex flex-row items-center gap-2 rounded-full bg-black/80 py-2 pr-6 pl-2">
-            <img src={teamMember?.avatarSrc || '/images/avatarplaceholder.svg'} alt="Profile" class=" h-12 w-12 rounded-full" />
+            <img src={teamMember?.avatarSrc || '/images/avatarplaceholder.svg'} alt="Profile" class=" h-11 w-11 rounded-full" />
             <div class="flex flex-col">
                 <span class="leading-5 font-medium">{teamMember?.realName || 'No name lol'}</span>
                 <span class="text-sm text-white/80">{teamMember?.username || author || 'Please add an author'}</span>
