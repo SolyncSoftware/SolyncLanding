20d598b4d22c18a6e502b9af32d615b7752d9701
papertek <littlerock894@gmail.com>
2026-06-21 22:25:18 -0500

    style: large posts and small posts (wip

--- src/lib/components/blog/LargePost.svelte
+++ src/lib/components/blog/LargePost.svelte
@@ -12,7 +12,7 @@
     const { title, description, link, category, image }: Props = $props();
 </script>
 
-<div class="font-display border-accent relative border-b-12 bg-white">
+<!-- <div class="font-display border-accent relative border-b-12 bg-white">
     <img loading="lazy" src={image || '/images/fallback.png'} alt="screenshot of news post" class="h-92 w-full object-cover" />
     <div class="bg-accent absolute ml-8 -translate-y-6 justify-self-start px-2 py-1 text-xl font-bold text-white uppercase">{category}</div>
     <div class="flex flex-col justify-between p-8 xl:flex-row">
@@ -23,5 +23,22 @@
             </p>
         </div>
         <ButtonSimple text="READ MORE" href={link} class="self-end text-lg" />
+    </div>
+</div> -->
+
+<div class="border-offwhite flex flex-col overflow-hidden rounded-4xl bg-white/80 shadow-xl/4 transition">
+    <img loading="lazy" src={image || '/images/fallback.png'} alt={`${image}`} class="h-92 w-full rounded-4xl object-cover p-1" />
+    <div
+        class="absolute ml-5 translate-y-6 justify-self-start rounded-full bg-black/80 px-4 py-2 text-xl font-bold text-white first-letter:uppercase"
+    >
+        {category}
+    </div>
+    <div class="flex h-full flex-col px-6 pt-1 pb-6 text-lg">
+        <h3 class="text-accent text-3xl font-bold">{title}</h3>
+        <p class="line-clamp-3 text-xl leading-5">{description}</p>
+
+        <div class="mt-auto flex gap-4">
+            <ButtonSimple text="Read more" href={link} class="mt-4 self-start text-lg" />
+        </div>
     </div>
 </div>
--- src/lib/components/blog/SmallPost.svelte
+++ src/lib/components/blog/SmallPost.svelte
@@ -12,7 +12,7 @@
     const { title, description, link, category, image }: Props = $props();
 </script>
 
-<div class="font-display border-accent relative max-h-146 border-b-12 bg-black/75">
+<!-- <div class="font-display border-accent relative max-h-146 border-b-12 bg-black/75">
     <img loading="lazy" src={image || '/images/fallback.png'} alt="..." class="h-56 w-full object-cover" />
     <div class="bg-accent absolute ml-5 -translate-y-6 justify-self-start px-2 py-1 text-xl font-bold text-black uppercase">{category}</div>
     <div class="grid h-[calc(100%-14rem)] grid-rows-[1fr_auto] gap-6 p-6">
@@ -21,5 +21,22 @@
             <p class="line-clamp-2 text-lg">{description}</p>
         </div>
         <ButtonSimple text="READ MORE" href={link} class="ml-auto w-fit justify-self-end text-lg" />
+    </div>
+</div> -->
+
+<div class="border-offwhite flex flex-col overflow-hidden rounded-4xl bg-white/80 shadow-xl/4 transition">
+    <img loading="lazy" src={image || '/images/fallback.png'} alt={`${image}`} class="h-55 w-full rounded-4xl object-cover p-1" />
+    <div
+        class="absolute ml-5 translate-y-6 justify-self-start rounded-full bg-black/80 px-4 py-2 text-xl font-bold text-white first-letter:uppercase"
+    >
+        {category}
+    </div>
+    <div class="flex h-full flex-col px-6 pt-1 pb-6 text-lg">
+        <h3 class="text-accent text-2xl font-bold">{title}</h3>
+        <p class="line-clamp-3 leading-5">{description}</p>
+
+        <div class="mt-auto flex gap-4">
+            <ButtonSimple text="Read more" href={link} class="mt-4 self-start text-lg" />
+        </div>
     </div>
 </div>
