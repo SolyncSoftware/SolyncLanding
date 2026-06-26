5ed8cf2d34836d71d3c89211bfcb91f4915918ce
papertek <littlerock894@gmail.com>
2026-06-25 19:51:15 -0500

    style: design updates

--- src/lib/components/mdsvex_layouts/BlogLayout.svelte
+++ src/lib/components/mdsvex_layouts/BlogLayout.svelte
@@ -52,8 +52,8 @@
     <h1 class="text-accent ml-9 truncate text-4xl font-black text-wrap xl:text-5xl">{title}</h1>
     <div class="mb-8 ml-9 flex flex-row items-center gap-4">
         <span class="text-2xl font-black text-black/90">{Formatting.formatDate(new Date(date).getTime() / 1000)}</span>
-        <span class="h-fit rounded-full bg-black/90 p-1"></span>
-        <span class="text-lg text-black/80">{categories.join(', ')}</span>
+        <span class="h-fit rounded-full bg-black/90 p-0.75"></span>
+        <span class="text-lg text-black/90">{categories.join(', ')}</span>
     </div>
 
     <div class="markdown rounded-4xl bg-white p-9 text-2xl">
