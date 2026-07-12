420472a83c14fe5cdc21b170467840782dea0d2f
papertek <littlerock894@gmail.com>
2026-07-11 23:54:35 -0500

    feat: begin about us cleanup

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -3,12 +3,12 @@
     import OurTeam from '$lib/components/index/OurTeam.svelte';
 </script>
 
-<h1 class="text-5xl font-bold">ABOUT US</h1>
+<h1 class="text-5xl font-bold text-white">About us</h1>
 <div class="mt-6 flex max-w-200 flex-col gap-4 text-xl">
     <p>
-        <span class="text-accent font-bold">SOLYNC</span> is a software collective driven by simple principles: putting users first, challenging
-        tradition with originality, and constantly improving. We don't sell out, make empty promises, or cut corners for profit. What we create
-        is a direct reflection of that commitment.
+        <span class="font-bold">SOLYNC</span> is a software collective driven by simple principles: putting users first, challenging tradition
+        with originality, and constantly improving. We don't sell out, make empty promises, or cut corners for profit. What we create is a direct
+        reflection of that commitment.
     </p>
     <p>
         We iterate quickly and thoughtfully, refining what works and fixing what doesn't. Continuous growth means owning our impact and
@@ -24,13 +24,4 @@
     </p>
     <p class="text-lg opacity-90">The partner program is currently closed, but we will reopen applications in the near future.</p>
     <OurPartners />
-</div>
-
-<div class="mt-24 flex max-w-301 flex-col gap-4 text-xl">
-    <h1 class="text-5xl font-bold">OUR TEAM</h1>
-    <p class="text-lg opacity-90">
-        Our company is flat by design! We prioritize collectivism, empathy, and mutual respect to ensure that every member has a direct say
-        in what we make.
-    </p>
-    <OurTeam />
 </div>
