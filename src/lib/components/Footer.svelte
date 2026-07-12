546f78c75c454544305342943e719175b6323c73
papertek <littlerock894@gmail.com>
2026-07-11 23:00:10 -0500

    feat: bluesky

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -34,7 +34,7 @@
     let socials = $state([
         { icon: SiDiscord, href: 'https://discord.gg/nUeRyRtDYC' },
         // { icon: SiYoutube, href: 'https://youtube.com/@SolyncCorp' },
-        // { icon: SiBluesky, href: 'https://bsky.app/profile/solync.org' },
+        { icon: SiBluesky, href: 'https://bsky.app/profile/solync.org' },
         { icon: SiGithub, href: 'https://github.com/SolyncSoftware' }
     ]);
 </script>
