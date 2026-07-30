ef80b2a5fcb30420ceb2078d74f6786d3b535b1e
papertek <littlerock894@gmail.com>
2026-07-30 01:19:03 -0500

    chore(apply page): once more

--- src/routes/apply/+page.svelte
+++ src/routes/apply/+page.svelte
@@ -31,7 +31,7 @@
             <ul class="ml-6 list-disc">
                 <li class="mb-4">An organization where the environment and schedule is extremely flexible.</li>
                 <li class="mb-4">
-                    Opportunity to collaborate with experienced members to gain insight, inspiration, and expanding skillsets.
+                    Opportunity to collaborate with experienced members to gain insight, inspiration, and expansive skillsets.
                 </li>
 
                 <li class="mb-4">
