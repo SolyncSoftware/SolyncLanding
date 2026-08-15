5ba99df3e0cfee341ff6633dbdab59d9b2d9b9bd
papertek <littlerock894@gmail.com>
2026-08-15 16:15:32 -0500

    feat: more titles to pages

---
+++ src/routes/donate/+page.server.ts
@@ -0,0 +1,9 @@
+export function load() {
+    return {
+        meta: {
+            title: 'Solync / Support us',
+            description:
+                "Solync is a software startup based in Texas, United States. We're a collective of thinkers who like to showcase our work while staying ethical to the end user. We run scalable services (StreamSkinner, Gardens Wiki) without any CEO, board, or hierarchy. If you send us money, you'll help us scale and build a better Solync."
+        }
+    };
+}
