303915c2c49631c3d57fc30fccf98d8c3db6f7e4
papertek <littlerock894@gmail.com>
2026-08-17 00:18:08 -0500

    fix: fix small text

--- src/routes/donate/+page.svelte
+++ src/routes/donate/+page.svelte
@@ -9,16 +9,16 @@
             <!-- Text Column -->
             <div class="max-w-210 font-medium">
                 <h1 class="text-4xl font-bold md:text-6xl">Build a better Solync.</h1>
-                <p class="mt-4 text-lg md:text-2xl">
+                <p class="mt-4 text-2xl">
                     Solync is a software startup based in Texas, United States. We're a collective of thinkers who like to showcase our work
                     while staying ethical to the end user. We run scalable services (StreamSkinner, Gardens Wiki) without any CEO, board, or
                     hierarchy. If you send us money, you'll help us scale and build a better Solync.
                 </p>
-                <p class="mt-4 text-lg md:text-2xl">
+                <p class="mt-4 text-2xl">
                     Our greatest priority is giving back to users and empowering them with cool stuff, whether it be software or something
                     else we find interesting. We wish to create a pleasant end-user experience that's sustainable, ethical, and really cool!
                 </p>
-                <p class="mt-4 mb-6 text-lg md:text-2xl">
+                <p class="mt-4 mb-6 text-2xl">
                     We need a way to scale to build better software, and funding us helps keep Solync on that path towards sustainability.
                     Your contributions are greatly appreciated and will not go to waste. We believe in transparency, so we plan to build a
                     platform where users can view all of our donations, earnings, and more!
