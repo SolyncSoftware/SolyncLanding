babce3e547214e84a58b2ae02eee0b3bef35ee26
papertek <littlerock894@gmail.com>
2026-08-30 23:14:22 -0500

    fix: fix buttons not styling correctly

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -9,8 +9,8 @@
         class={`bg-offaccent group button flex
         items-center gap-4 self-end rounded-full
         px-8 py-3 text-xl text-white shadow-xl/6
-        transition-all ease-in-out enabled:hover:cursor-pointer
-        enabled:hover:bg-black disabled:opacity-50
+        transition-all ease-in-out hover:cursor-pointer
+        hover:bg-black disabled:opacity-50
         ${className}`}
         {...rest}
     >
@@ -22,8 +22,8 @@
         class={`bg-offaccent group button flex
         items-center gap-4 self-end rounded-full
         px-8 py-3 text-xl text-white shadow-xl/6
-        transition-all ease-in-out enabled:hover:cursor-pointer
-        enabled:hover:bg-black disabled:opacity-50
+        transition-all ease-in-out hover:cursor-pointer
+        hover:bg-black disabled:opacity-50
         ${className}`}
         {...rest}
     >
