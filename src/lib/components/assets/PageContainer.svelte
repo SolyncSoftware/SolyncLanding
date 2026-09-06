ffb238f8ee8f719b4cf5b013365bb7c72ed2333b
papertek <littlerock894@gmail.com>
2026-09-05 22:22:01 -0500

    fix: fix border rad

--- src/lib/components/assets/PageContainer.svelte
+++ src/lib/components/assets/PageContainer.svelte
@@ -27,10 +27,13 @@
     });
 </script>
 
-<div bind:this={pageContainerEl} class="relative z-100 mx-0 border-white {containerClass} rounded-4xl border-8 text-white 2xl:-mx-10">
+<div
+    bind:this={pageContainerEl}
+    class="border-offwhite relative z-100 mx-0 overflow-hidden rounded-4xl border-8 text-white 2xl:-mx-10 {containerClass}"
+>
     <div bind:this={waveSlotEl} class="absolute inset-0 z-0 h-full w-full rounded-4xl"></div>
-    <div class="absolute inset-0 z-1 rounded-4xl bg-black/40"></div>
-    <div class="{className} absolute inset-0 z-5 rounded-4xl mix-blend-plus-lighter"></div>
+    <div class="absolute inset-0 z-1 bg-black/40"></div>
+    <div class="{className} absolute inset-0 z-5 mix-blend-plus-lighter"></div>
     <div class="relative z-10 p-9 {innerClass}">
         {@render children()}
     </div>
