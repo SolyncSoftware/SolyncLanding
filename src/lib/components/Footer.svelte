c51b9d7ddb28626ab72bc1be1a06c1e1f79311c8
papertek <littlerock894@gmail.com>
2026-09-08 20:47:41 -0500

    feat: uncomment the legal pages in footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -25,8 +25,8 @@
         {
             title: 'More',
             links: [
-                // { name: 'Privacy Policy', href: '/legal/privacy' },
-                // { name: 'Terms of Service', href: '/legal/terms' },
+                { name: 'Privacy Policy', href: '/legal/privacy' },
+                { name: 'Terms of Service', href: '/legal/terms' },
                 { name: 'Contact us', href: '/contact' }
             ]
         }
