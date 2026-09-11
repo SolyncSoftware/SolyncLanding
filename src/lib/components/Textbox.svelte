a84d7b081b8cfc0207ec44e44569662c088e8e4e
papertek <littlerock894@gmail.com>
2026-09-11 00:03:15 -0500

    fix: fix textarea not growing

--- src/lib/components/Textbox.svelte
+++ src/lib/components/Textbox.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     let input = $state('world');
 
-    let { class: className = '', type = 'text', placeholder = 'Text...', value = $bindable(""), required = false, ...rest} = $props();
+    let { class: className = '', type = 'text', placeholder = 'Text...', value = $bindable(''), required = false, ...rest } = $props();
 
     function grow(node: HTMLTextAreaElement) {
         function adjust() {
@@ -19,30 +19,29 @@
 </script>
 
 {#if type === 'textarea'}
-<textarea
-    // use:grow // jank and stupid
-    bind:value
-    {...rest}
-    class={`w-full min-w-0 focus:ring-accent bg-offwhite resize-none overflow-clip rounded-full
+    <textarea
+        use:grow
+        bind:value
+        {...rest}
+        class={`focus:ring-accent bg-offwhite w-full min-w-0 resize-none overflow-clip rounded-full
     px-5 py-4 font-sans text-lg focus:ring-2 focus:outline-none
     ${className}`}
-    id="fname"
-    name="fname"
-    required={required}
-    {placeholder}
-></textarea>
+        id="fname"
+        name="fname"
+        {required}
+        {placeholder}
+    ></textarea>
 {:else}
-<input
-    // use:grow // jank and stupid
-    bind:value
-    {...rest}
-    type={type}
-    class={`w-full min-w-0 inline-block focus:ring-accent bg-offwhite resize-none overflow-clip rounded-full
+    <input
+        bind:value
+        {...rest}
+        {type}
+        class={`focus:ring-accent bg-offwhite inline-block w-full min-w-0 resize-none overflow-clip rounded-full
     px-5 py-4 font-sans text-lg focus:ring-2 focus:outline-none
     ${className}`}
-    id="fname"
-    name="fname"
-    required={required}
-    {placeholder}
-/>
+        id="fname"
+        name="fname"
+        {required}
+        {placeholder}
+    />
 {/if}
