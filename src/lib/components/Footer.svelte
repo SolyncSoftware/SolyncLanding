f294bd1bf4c0bda89abf1baecc1c6c3eb56e6846
papertek <littlerock894@gmail.com>
2026-09-16 21:15:45 -0500

    feat: add solync status to footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -27,8 +27,8 @@
             links: [
                 // { name: 'Privacy Policy', href: '/legal/privacy' },
                 // { name: 'Terms of Service', href: '/legal/terms' },
-                { name: 'Contact us', href: '/contact' },
-                { name: 'Status Page', href: 'https://status.solync.org', external: true }
+                { name: 'Solync Status', href: 'https://status.solync.org', external: true },
+                { name: 'Contact us', href: '/contact' }
             ]
         }
     ]);
