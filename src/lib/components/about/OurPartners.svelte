9320b7c16b3263e08d70bcdd36dcef3d48b533ab
papertek <littlerock894@gmail.com>
2026-09-20 21:47:50 -0500

    feat(about us): :sparkles: refresh the about page

--- src/lib/components/about/OurPartners.svelte
+++ src/lib/components/about/OurPartners.svelte
@@ -17,23 +17,18 @@
 </script>
 
 <section>
-    <div class="grid grid-cols-1 gap-5 md:grid-cols-3">
+    <div class="grid grid-cols-1 gap-10 md:grid-cols-5">
         {#each partners as partner}
             {#snippet partnerImg()}
                 <enhanced:img src={partner.src} alt={partner.alt} class="h-full w-full object-contain" />
             {/snippet}
 
             {#if partner.href}
-                <a
-                    href={partner.href}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    class="group hover:bg-offaccent relative flex items-center rounded-2xl bg-black p-10 transition-all"
-                >
+                <a href={partner.href} target="_blank" rel="noopener noreferrer" class="relative flex items-center">
                     {@render partnerImg()}
                 </a>
             {:else}
-                <div class="bg-accent relative flex items-center p-10">
+                <div class="bg-accent relative flex items-center">
                     {@render partnerImg()}
                 </div>
             {/if}
