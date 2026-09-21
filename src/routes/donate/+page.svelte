7edddf7a7f8e061dbbcc8ccb6b9ca9f6ded7be5c
papertek <littlerock894@gmail.com>
2026-09-21 11:08:47 -0500

    chore: add black font to donate page

--- src/routes/donate/+page.svelte
+++ src/routes/donate/+page.svelte
@@ -8,7 +8,7 @@
         <div class="flex flex-col gap-8 xl:flex-row xl:items-start xl:justify-between">
             <!-- Text Column -->
             <div class="max-w-210 font-medium">
-                <h1 class="text-4xl md:text-6xl">Build a better Solync.</h1>
+                <h1 class="text-4xl font-black md:text-6xl">Build a better Solync.</h1>
                 <p class="mt-4 text-2xl">
                     Solync is a software startup based in Texas, United States. We're a collective of thinkers who like to showcase our work
                     while staying ethical to the end user. We run scalable services (StreamSkinner, Gardens Wiki) without any CEO, board, or
