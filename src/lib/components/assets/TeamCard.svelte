f52ca3c84739ec83c5ef91bb46a8c510774e10c1
papertek <littlerock894@gmail.com>
2026-09-22 01:40:07 -0500

    style: refresh team card design

--- src/lib/components/assets/TeamCard.svelte
+++ src/lib/components/assets/TeamCard.svelte
@@ -39,8 +39,8 @@
             alt={member.realName}
         />
         <div class="flex flex-col space-y-1 text-left transition">
-            <p class="text-accent text-3xl leading-[1em] font-bold md:text-left md:text-4xl">{member.username}</p>
-            <p class="text-xl leading-[1em] font-light sm:text-2xl md:text-left">{member.realName}</p>
+            <p class="text-accent text-3xl leading-[1em] font-black md:text-left md:text-4xl">{member.username}</p>
+            <p class="text-xl leading-[1em] sm:text-2xl md:text-left">{member.realName}</p>
         </div>
 
         <div class="ml-auto flex flex-row items-center gap-4 text-black">
