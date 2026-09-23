2e2dee869024dbef62ff0b32124301da1afae59e
almahbuby <almahbubyanwar@protonmail.com>
2026-09-23 15:08:21 +0700

    Implemented initial footer redesign. TODO: Add graphics for logo on the bottom

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -4,24 +4,25 @@
     import Button from './Button.svelte';
     import ButtonNew from './ButtonNew.svelte';
 
-    let footerSections = $state([
+    let footerSections = [
         {
-            title: 'Solync',
+            title: 'Navigation',
             links: [
+                { name: 'Home', href: '/' },
                 { name: 'About us', href: '/about' },
                 { name: 'Sponsor us', href: '/donate' },
                 { name: 'Our Blog', href: '/blog' }
             ]
         },
         {
-            title: 'Projects',
-            links: [
-                // { name: 'StreamSkinner', href: 'https://streamskinner.com/', external: true },
-                { name: 'Gardens Wiki', href: 'https://gardens.wiki/', external: true },
-                { name: 'Potaro Wiki', href: 'https://github.com/SolyncSoftware/porter-wiki-frontend', external: true },
-                { name: 'All Projects', href: '/about#our-work' }
-                // { name: 'Solync Orbit', href: 'https://orbit.solync.org/' }
-            ]
+            // title: 'Projects',
+            // links: [
+            //     // { name: 'StreamSkinner', href: 'https://streamskinner.com/', external: true },
+            //     { name: 'Gardens Wiki', href: 'https://gardens.wiki/', external: true },
+            //     { name: 'Potaro Wiki', href: 'https://github.com/SolyncSoftware/porter-wiki-frontend', external: true },
+            //     { name: 'All Projects', href: '/about#our-work' }
+            //     // { name: 'Solync Orbit', href: 'https://orbit.solync.org/' }
+            // ]
         },
         {
             title: 'More',
@@ -32,124 +33,58 @@
                 { name: 'Contact us', href: '/contact' }
             ]
         }
-    ]);
+    ];
 
-    let socials = $state([
+    let socials = [
         { icon: SiDiscord, href: 'https://discord.gg/nUeRyRtDYC' },
         { icon: SiYoutube, href: 'https://youtube.com/@SolyncSoftware' },
         { icon: SiBluesky, href: 'https://bsky.app/profile/solync.org' },
         { icon: SiGithub, href: 'https://github.com/SolyncSoftware' }
-    ]);
+    ];
 </script>
 
-<footer class="bg-black px-5 py-12 text-xl text-white 2xl:px-70">
-    <!-- Donate banner -->
-    <a
-        class="group mb-18 flex flex-col items-center justify-between gap-4 rounded-2xl bg-black px-6 py-6 transition hover:bg-pink-500 sm:flex-row sm:px-12"
-        href="/donate"
-    >
-        <div class="text-center sm:text-left">
-            <p class="text-2xl font-bold sm:text-4xl">Software for everyone since 2024</p>
-            <p class="text-base sm:text-xl">Support us by donating or becoming a sponsor</p>
-        </div>
-        <span class="heart text-error inline-block text-3xl transition group-hover:text-white sm:text-4xl" aria-hidden="true">&#10084;</span
-        >
-    </a>
-
-    <!-- Three bold statements -->
-    <div class="mb-18 flex flex-col gap-4 text-2xl font-light sm:text-4xl md:text-6xl">
-        <p class=" w-full">We're an independent software collective</p>
-        <p class="w-full">creating user-first experiences.</p>
-        <p class="w-full font-black">Building what comes next, together.</p>
-    </div>
-
-    <!-- Main footer grid -->
-    <div class="grid grid-cols-1 gap-8 lg:grid-cols-2">
-        <!-- Left side -->
-        <div class="flex flex-col items-center gap-4 sm:items-start">
-            <a href="/" class="group block w-fit">
-                <SolyncLogo
-                    class="group-hover:fill-accent w-60 fill-white transition-all duration-300 ease-in-out group-hover:scale-102 sm:w-80"
-                />
-            </a>
-
-            <div class="mt-auto flex w-full flex-col items-center gap-4 text-center font-bold sm:flex-row sm:text-left">
-                <ButtonNew href="/apply">
-                    <span class="leading-5 font-black">Join us on our journey!</span><span class="leading-5">Apply now</span>
-                </ButtonNew>
-            </div>
-
-            <ul class="flex flex-row items-center gap-4">
+<footer class="bg-deepblack text-xl text-white">
+    <div class="bg-accent space-y-12 px-5 py-6 pb-5 2xl:px-70">
+        <p class="text-4xl text-white">
+            We're an independent software collective creating user-first experiences.<br />
+            <span class="font-black">We collaborate to create what's next.</span>
+        </p>
+        <section class="flex flex-row items-center justify-between">
+            <div class="flex flex-row gap-4">
                 {#each socials as { icon: Icon, href }}
-                    <li>
-                        <a
-                            {href}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            aria-label={'Social link'}
-                            class="hover:text-accent flex items-center justify-center transition"
-                        >
-                            <Icon class="h-7 w-7 transition sm:h-6 sm:w-6" />
-                        </a>
-                    </li>
-                {/each}
-            </ul>
-        </div>
-
-        <!-- Right side -->
-        <div class="flex flex-col items-center gap-4 sm:items-end">
-            <div class="flex flex-wrap justify-center gap-8 md:gap-14 xl:justify-end">
-                {#each footerSections as section}
-                    <div class="flex flex-col gap-4 text-center sm:text-left">
-                        <span class="text-accent w-auto font-black">
-                            {section.title}
-                        </span>
-                        <nav class="flex flex-col gap-3 text-white/75 sm:gap-4">
-                            {#each section.links as link}
-                                <a
-                                    href={link.href}
-                                    class="footer-links hover:text-accent transition-colors"
-                                    target={link.external ? '_blank' : undefined}
-                                    rel={link.external ? 'noopener noreferrer' : undefined}
-                                >
-                                    {link.name}
-                                </a>
-                            {/each}
-                        </nav>
-                    </div>
+                    <a {href}><Icon class="h-6 w-auto" /></a>
                 {/each}
             </div>
-
-            <div class="flex flex-row flex-wrap items-center justify-center gap-3">
-                <img src="/images/badges/csshard.gif" alt="css is hard" class="w-auto" loading="lazy" />
-                <img src="/images/badges/powered-by-debian.gif" alt="powered by debian" class="w-auto" loading="lazy" />
-                <a href="https://brainmade.org/" target="_blank">
-                    <img src="/images/badges/brainmade.png" alt="90% human made" class="w-auto" loading="lazy" />
-                </a>
+            <div class="flex flex-row flex-wrap justify-end gap-2">
+                <img src="/images/badges/brainmade.png" alt="brainmade.org" class="h-8 w-auto" />
+                <img src="/images/badges/csshard.gif" alt="CSS is difficult" class="h-8 w-auto" />
+                <img src="/images/badges/powered-by-debian.gif" alt="Powered by Debian" class="h-8 w-auto" />
             </div>
-
-            <div class="text-center text-sm sm:text-xl">
-                &copy; {new Date().getFullYear()} Solync. Made with
-                <span class="heart text-error inline-block" aria-hidden="true">&#10084;</span>
-                in Texas.
-            </div>
-        </div>
+        </section>
+    </div>
+    <div class="flex flex-col gap-3 px-5 py-6 lg:flex-row 2xl:px-70 [&_p]:text-xl [&_p]:leading-[1em]">
+        {#each footerSections as { title, links }}
+            <section
+                class="[&_p]:font-blue [&>*:first-child]:text-accent flex flex-col gap-3 leading-[1em] text-white/70 lg:gap-2 [&_a]:hover:text-white [&>*:first-child]:font-black"
+            >
+                <p>{title}</p>
+                <div class="flex flex-col gap-3 lg:flex-row lg:gap-4">
+                    {#each links as { name, href }}
+                        <p><a {href}>{name}</a></p>
+                    {/each}
+                </div>
+            </section>
+        {/each}
+        <section
+            class="mt-6 flex flex-row flex-wrap justify-between gap-2 leading-[1em] text-white lg:mt-0 lg:ml-auto lg:flex-col lg:justify-start lg:text-end"
+        >
+            <p class="text-accent font-black">&copy; 2026 Solync</p>
+            <p class="text-white">Made with <span class="pulse text-red-400">&#10084;</span> in Texas</p>
+        </section>
     </div>
 </footer>
 
 <style scoped>
-    footer {
-        background-image: linear-gradient(to bottom, rgb(0 0 0 / 0%) 0%, #000000 76%), url(/images/articles/fallback.png);
-        background-size: cover;
-        &:img {
-            display: block;
-        }
-    }
-
-    .heart {
-        animation: pulse 2s ease infinite;
-    }
-
     @keyframes pulse {
         0%,
         100% {
